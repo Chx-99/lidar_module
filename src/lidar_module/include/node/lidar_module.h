b665db36aa0bc7a65a26780a51f01f7ed0a93a2b
chx <945593791@qq.com>
2025-12-29 09:07:47 +0800

    最新

--- src/lidar_module/include/node/lidar_module.h
+++ src/lidar_module/include/node/lidar_module.h
@@ -3,22 +3,42 @@
 #include "lidar/lidar_base.h"
 #include "lidar/lidar_scan.h"
 #include "concurrentqueue.h"
-#include "process/data_analysis.h"
+#include "process/process_work.h"
 
 #include <iomanip>
 #include <iostream>
 #include <thread>
 #include <vector>
+#include <unordered_map>
 #include <rclcpp/rclcpp.hpp>
 
+namespace lidar_manager
+{
+    using namespace lidar_base;
+    using namespace lidar_scanner;
+    using namespace base_frame;
+    using namespace frame_tools;
+    using namespace free_queue;
+    using namespace process_worker;
 
-namespace lidar_manager{
-
-    class LidarManager {
+    class LidarManager
+    {
     public:
         LidarManager() = default;
         ~LidarManager() = default;
+
     private:
+        boost::asio::io_context io_context;
+        boost::asio::executor_work_guard<boost::asio::io_context::executor_type> work_guard;
+        // 雷达扫描器
+        LidarScanner scanner;
+        // 雷达设备映射表
+        std::unordered_map<std::string, std::unique_ptr<lidar_base::Lidar>> lidar_devices;
+        // 数据处理器
+        ProcessorThreadPool processor;
+        // IO线程池
+        std::vector<std::thread> io_threads;
+        
     };
 
 };
